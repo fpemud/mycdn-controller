74015f08d77af0b825964039624c93cac5b5cca7
fpemud <fpemud@sina.com>
2020-07-17 12:59:53 +0800

    fix

--- lib/mc_advertiser.py
+++ lib/mc_advertiser.py
@@ -13,42 +13,6 @@ class McAdvertiser:
     def __init__(self, param):
         self.param = param
 
-        bHasHttpMirrorSite = False
-        bHasFtpMirrorSite = False
-        bHasRsyncMirrorSite = False
-        bHasGitHttpMirrorSite = False
-        for ms in self.param.mirrorSiteDict.values():
-            for proto in ms.advertiseProtocolList:
-                if proto == "http":
-                    bHasHttpMirrorSite = True
-                elif proto == "ftp":
-                    bHasFtpMirrorSite = True
-                elif proto == "rsync":
-                    bHasRsyncMirrorSite = True
-                elif proto == "git-http":
-                    bHasGitHttpMirrorSite = True
-                else:
-                    assert False
-
-        self.httpServer = None
-        self.ftpServer = None
-        self.rsyncServer = None
-        if bHasHttpMirrorSite or bHasGitHttpMirrorSite:
-            if self.param.httpPort == "random":
-                self.param.httpPort = McUtil.getFreeSocketPort("tcp")
-            self.httpServer = McHttpServer(self.param, self.param.mainloop, self.param.listenIp, self.param.httpPort, McConst.logDir)
-            self.httpServer.start()
-        if bHasFtpMirrorSite:
-            if self.param.ftpPort == "random":
-                self.param.ftpPort = McUtil.getFreeSocketPort("tcp")
-            self.ftpServer = McFtpServer(self.param.mainloop, self.param.listenIp, self.param.ftpPort, McConst.logDir)
-            self.ftpServer.start()
-        if bHasRsyncMirrorSite:
-            if self.param.rsyncPort == "random":
-                self.param.rsyncPort = McUtil.getFreeSocketPort("tcp")
-            self.rsyncServer = McRsyncServer(self.param.mainloop, self.param.listenIp, self.param.rsyncPort, McConst.tmpDir, McConst.logDir)   # FIXME
-            self.rsyncServer.start()
-
     def advertiseMirrorSite(self, mirrorSiteId):
         msObj = self.param.mirrorSiteDict[mirrorSiteId]
         if "http" in msObj.advertiseProtocolList:
@@ -62,12 +26,4 @@ class McAdvertiser:
             pass
 
     def dispose(self):
-        if self.httpServer is not None:
-            self.httpServer.stop()
-            self.httpServer = None
-        if self.ftpServer is not None:
-            self.ftpServer.stop()
-            self.ftpServer = None
-        if self.rsyncServer is not None:
-            self.rsyncServer.stop()
-            self.rsyncServer = None
+        pass
